c141b931d2e11f7ce534b40d20fb4bf800a609a0
nayab gul <nayabgul258@gmail.com>
2024-11-26 23:39:54 +0530

    Convert ll to Arr

--- Linkedlist.java
+++ Linkedlist.java
@@ -15,10 +15,29 @@ class Node{
  };  
 
 public class Linkedlist {
+  private static  Node covertArr2LL(int[] arr){
+    Node head = new Node(arr[0]);
+    Node mover  = head;
+    for(int i= 1; i < arr.length; i++){
+      Node temp = new Node(arr[i]);
+      mover.next = temp;
+      mover = temp;
+    }
+    return head;
+  }
+  private static  void print(Node head){
+      while(head != null){
+        System.out.print(head.data + " ");
+        head = head.next;
+      }
+      System.out.println();
+  }
+
+
   public static void main(String args[]){
     int arr [] = {4,6,2,7};
-    Node Arr = new Node(arr[3]);
-    System.out.print(Arr.data); 
+    Node head = covertArr2LL(arr);
+    print(head);
   }
 };
 
