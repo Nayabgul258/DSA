148d78cfa2e20c91635533c4c29d44eb08b2bf3f
nayab gul <nayabgul258@gmail.com>
2024-11-30 23:47:27 +0530

    Stirng Program

---
+++ Strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = {'a','v','f'};
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
